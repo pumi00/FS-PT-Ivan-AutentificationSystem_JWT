a908a70a75de44eca784fc5552608f2d49bab759
Ivan Perez <pumi0751@gmail.com>
2025-01-09 12:18:56 +0000

    feat: adding flux sections and formulary

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -16,6 +16,68 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
+			
+			getUserData: async () =>{try{
+
+				const resp = await fetch('https://verbose-guide-wr9v5p7rvqvgf566r-3001.app.github.dev/api/protected',{
+					method: 'GET',
+					headers: {
+						'Content-Type' : 'application/json',
+						'Authorization': `Bearer ${localStorage.getItem('token')}`
+					},
+					body: JSON.stringify(formData)
+				})
+				if(!resp.ok) throw new Error('Error registering')
+				const data = await resp.json()
+				console.log(data)
+				localStorage.setItem('token', data.token)
+				setStore({user: data.user})
+			}
+			catch (error){
+				console.error(error)
+			}
+		},
+		register: async formData => {
+
+			try{
+
+				const resp = await fetch('https://verbose-guide-wr9v5p7rvqvgf566r-3001.app.github.dev/api/register',{
+					method: 'POST',
+					headers: {
+						'Content-Type' : 'application/json'
+					},
+					body: JSON.stringify(formData)
+				})
+				if(!resp.ok) throw new Error('Error registering')
+				const data = await resp.json()
+				console.log(data)
+				localStorage.setItem('token', data.token)
+				setStore({auth: true, token: data.token})
+			}
+			catch (error){
+				console.error(error)
+			}
+		},
+		login: async formData => {
+
+			try{
+				const resp = await fetch('https://verbose-guide-wr9v5p7rvqvgf566r-3001.app.github.dev/api/login',{
+					method: 'POST',
+					headers: {
+						'Content-Type' : 'application/json'
+					},
+					body: JSON.stringify(formData)
+				})
+				if(!resp.ok) throw new Error('Error registering')
+				const data = await resp.json()
+				console.log(data)
+				localStorage.setItem('token', data.token)
+				setStore({auth: true, token: data.token})
+			}
+			catch (error){
+				console.error(error)
+			}
+		},
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
